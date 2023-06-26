ee577e8e4c71c1f3dbacf0bda3c12b39d6dcc1c7
HeYiJian <1004821187@qq.com>
2023-06-26 16:20:24 +0800

    裁剪头像

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -17,23 +17,23 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
-        return view('users.edit',compact('user'));
+        return view('users.edit', compact('user'));
     }
 
-    public function update(UserRequest $request,ImageUploadHandler $uploader,User $user)
+    public function update(UserRequest $request, ImageUploadHandler $uploader, User $user)
     {
         $data = $request->all();
 
-        if($request->avatar) {
-            $result = $uploader->save($request->avatar,'avatars',$user->id);
-            if($result){
+        if ($request->avatar) {
+            $result = $uploader->save($request->avatar, 'avatars', $user->id, 416);
+            if ($result) {
                 $data['avatar'] = $result['path'];
             }
         }
 
         $user->update($data);
 
-        return redirect()->route('users.show',$user->id)->with('success','个人资料更新成功！');
+        return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
 
 }
